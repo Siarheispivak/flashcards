c7b03c7bcc2bf8c40046108c8bd5a4a198a3142d
Siarheispivak <siarheispivak2@gmail.com>
2023-12-06 21:57:22 +0300

    feat:add typography.tsx, typography.stories.ts, typography.module.scss and index.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { Link } from 'react-router-dom'
+
+import { Button } from '@/components/ui/button'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <Button as={Link} to={'https://google.com'}>
+        Hello
+      </Button>
+    </div>
+  )
 }
