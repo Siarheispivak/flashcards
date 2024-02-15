35bbc36988cb3689e7a5ae2a1a0905767130f0d4
Siarheispivak <siarheispivak2@gmail.com>
2024-02-15 23:46:18 +0300

    fix: changed quotes at NEW_PASSWORD in routes.ts.

--- src/shared/const/routes.ts
+++ src/shared/const/routes.ts
@@ -2,7 +2,7 @@ export const routes = {
   AUTH: {
     CHECK_EMAIL: '/check-email',
     FORGOT_PASSWORD: '/recover-password',
-    NEW_PASSWORD: `/reset-password/:token`,
+    NEW_PASSWORD: '/reset-password/:token',
     SIGN_UP: '/sign-up',
     SING_IN: '/login',
   },
