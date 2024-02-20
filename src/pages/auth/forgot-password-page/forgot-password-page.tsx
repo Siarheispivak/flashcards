f003e4906c322f1eb1ac6b5bf398de5b09f1155a
Siarheispivak <siarheispivak2@gmail.com>
2024-02-20 20:37:23 +0300

    feat:setEmail action was added to the handleResetPassword.

--- src/pages/auth/forgot-password-page/forgot-password-page.tsx
+++ src/pages/auth/forgot-password-page/forgot-password-page.tsx
@@ -1,14 +1,18 @@
 import { useNavigate } from 'react-router-dom'
 
+import { appAction } from '@/app/model'
 import { ForgotPasswordForm } from '@/features/auth/ui'
+import { useActions } from '@/shared/lib/hooks/use-action'
 import { useRecoveryPasswordMutation } from '@/shared/services/auth-api'
 
 export const ForgotPasswordPage = () => {
   const [recoveryPassword] = useRecoveryPasswordMutation()
+  const { setEmail } = useActions(appAction)
   const navigate = useNavigate()
   const handleResetPassword = async (args: { email: string }) => {
     try {
       await recoveryPassword(args)
+      setEmail(args)
       navigate('/check-email')
     } catch (e) {
       console.error(e)
