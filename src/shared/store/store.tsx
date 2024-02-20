6cf04062b409b58a8ecea18dba1c46954135b5af
Siarheispivak <siarheispivak2@gmail.com>
2024-02-20 20:35:43 +0300

    feat:app reducer was added to the store.tsx

--- src/shared/store/store.tsx
+++ src/shared/store/store.tsx
@@ -1,3 +1,4 @@
+import { appSlice } from '@/app/model'
 import { decksSlice } from '@/features/decks/model'
 import { baseApi } from '@/shared/services/base-api/base-api'
 import { configureStore } from '@reduxjs/toolkit'
@@ -6,6 +7,7 @@ import { setupListeners } from '@reduxjs/toolkit/query/react'
 export const store = configureStore({
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
   reducer: {
+    app: appSlice,
     [baseApi.reducerPath]: baseApi.reducer,
     decks: decksSlice,
   },
