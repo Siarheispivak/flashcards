691fd61e956945658273d48aa60f843d20b982e8
Siarheispivak <siarheispivak2@gmail.com>
2024-02-27 23:55:52 +0300

    fix: DeckPagination component was uncommented.

--- src/pages/decks-pages/decks-page.tsx
+++ src/pages/decks-pages/decks-page.tsx
@@ -1,14 +1,17 @@
 import { DeckAdd } from '@/features/decks/ui/deck-add/deck-add'
+import { DeckPagination } from '@/features/decks/ui/deck-pagination'
 import { DeckSortBar } from '@/features/decks/ui/deck-sort-bar/deck-sort-bar'
 import { DeckTable } from '@/features/decks/ui/deck-table/deck-table'
 
+import s from './decks-page.module.scss'
+
 export const DecksPage = () => {
   return (
-    <div>
+    <div className={s.container}>
       <DeckAdd />
       <DeckSortBar />
       <DeckTable />
-      {/*<DeckPagination />*/}
+      <DeckPagination />
     </div>
   )
 }
