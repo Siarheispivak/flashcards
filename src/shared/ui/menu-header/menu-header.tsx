a913e42660821cd7afdf8391a7af694d44bca996
Siarheispivak <siarheispivak2@gmail.com>
2024-02-27 23:56:51 +0300

    feat: handleGoToProfile and useNavigate were added.

--- src/shared/ui/menu-header/menu-header.tsx
+++ src/shared/ui/menu-header/menu-header.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom'
+
 import { ProfileIcon, SignOutIcon } from '@/shared/assets'
 import { useLogOutMutation } from '@/shared/services/auth-api'
 import { Avatar } from '@/shared/ui/avatar'
@@ -13,12 +15,15 @@ type Props = {
 }
 export const MenuHeader = ({ email, name, src }: Props) => {
   const [logOut] = useLogOutMutation()
-
+  const navigate = useNavigate()
   const trigger = (
     <button className={s.trigger}>
       <Avatar src={src} />
     </button>
   )
+  const handleGoToProfile = () => {
+    navigate('/profile')
+  }
 
   return (
     <Dropdown trigger={trigger}>
@@ -29,7 +34,12 @@ export const MenuHeader = ({ email, name, src }: Props) => {
           <Typography variant={'caption'}>{email}</Typography>
         </div>
       </DropdownItem>
-      <DropdownItemWithIcon icon={<ProfileIcon />} text={'My profile'} />
+      <DropdownItemWithIcon
+        icon={<ProfileIcon />}
+        onClick={handleGoToProfile}
+        text={'My profile'}
+      />
+
       <DropdownItemWithIcon icon={<SignOutIcon />} onClick={() => logOut()} text={'Sign Out'} />
     </Dropdown>
   )
