23c390e9836f32447ca30b9704cb9a45c687e8fa
Siarheispivak <siarheispivak2@gmail.com>
2024-02-27 23:46:50 +0300

    feat: onAvatarInputBlur, profileName, saveChanges, cameraButton, containerInner and editPhoto - useState were added.

--- src/shared/ui/profile/profile-information.tsx
+++ src/shared/ui/profile/profile-information.tsx
@@ -1,7 +1,6 @@
 import { ChangeEvent, useState } from 'react'
 
 import { Camera, Edit, Logout } from '@/shared/assets/icons'
-import Avatar from '@/shared/assets/icons/avatar'
 import { Button } from '@/shared/ui/button'
 import { Card } from '@/shared/ui/card'
 import { TextField } from '@/shared/ui/text-field'
@@ -12,13 +11,14 @@ import s from './profile-information.module.scss'
 type Props = {
   avatar: string
   email: string
-  onAvatarChange: (newAvatar: string) => void
+  onAvatarChange: (avatar: string) => void
   onLogout: () => void
-  onNameChange: (newValue: string) => void
+  onNameChange: (value: string) => void
   value: string
 }
 
 export const ProfileInformation = ({
+  avatar,
   email,
   onAvatarChange,
   onLogout,
@@ -26,6 +26,7 @@ export const ProfileInformation = ({
   value,
 }: Props) => {
   const [editMode, setEditMode] = useState(false)
+  const [editPhoto, setEditPhoto] = useState(false)
   const [name, setName] = useState(value)
 
   const activateEditMode = () => {
@@ -35,6 +36,7 @@ export const ProfileInformation = ({
   const activateViewMode = () => {
     setEditMode(false)
     onNameChange(name)
+    console.log('new name added')
   }
   const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
     setName(e.currentTarget.value)
@@ -42,68 +44,86 @@ export const ProfileInformation = ({
 
   const handlerAvatarChanged = () => {
     onAvatarChange('new avatar')
+    setEditPhoto(true)
+    console.log('new avatar added')
   }
+
   const handleLogout = () => {
     onLogout()
   }
+  const onAvatarInputBlur = () => {
+    if (editPhoto) {
+      setEditPhoto(false)
+    }
+  }
 
   const profileName = editMode ? (
-    <TextField autoFocus onBlur={activateViewMode} onChange={changeTitle} value={name} />
+    <TextField
+      autoFocus
+      className={s.profileName}
+      onBlur={activateViewMode}
+      onChange={changeTitle}
+      value={name}
+    />
   ) : (
-    <Typography onDoubleClick={activateEditMode} variant={'h1'}>
-      {value}
-    </Typography>
+    <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }}>
+      <Typography onDoubleClick={activateEditMode} variant={'h1'}>
+        {value}
+      </Typography>
+      <Edit />
+    </div>
+  )
+  //вынести стили
+  //handler save changes
+  const saveChanges = editMode ? (
+    <Button className={s.saveChanges} variant={'primary'}>
+      Save Changes
+    </Button>
+  ) : (
+    <>
+      <Typography className={s.email} variant={'body2'}>
+        {email}
+      </Typography>
+      <Button onClick={handleLogout} variant={'secondary'}>
+        <Logout className={s.logout} />
+        Logout
+      </Button>
+    </>
+  )
+
+  const containerInner = editPhoto ? (
+    <input className={''} title={''} type={'file'} />
+  ) : (
+    <img alt={'avatar'} src={avatar} />
+  )
+  const cameraButton = !editPhoto ? (
+    <Button className={s.button} onClick={handlerAvatarChanged} variant={'secondary'}>
+      <Camera />
+    </Button>
+  ) : (
+    <></>
   )
 
   return (
-    <>
+    <div
+      onClick={onAvatarInputBlur}
+      style={{ alignItems: 'center', display: 'flex', height: '100vh', justifyContent: 'center' }}
+    >
       <Card className={s.card}>
         <Typography className={s.title} variant={'large'}>
           Personal Information
         </Typography>
         <div className={s.photoContainer}>
           <div className={s.containerInner}>
-            {/*<img src={avatar} alt="avatar"/>*/}
-            <Avatar className={s.avatar} />
-            <Button className={s.button} onClick={handlerAvatarChanged} variant={'secondary'}>
-              <Camera />
-            </Button>
+            {/*<img alt={'avatar'} src={avatar} />*/}
+            {/*<Avatar className={s.avatar} />*/}
+            {containerInner}
+            {cameraButton}
           </div>
         </div>
-        <div className={s.name}>
-          {profileName}
-          <Edit />
-        </div>
-        <Typography className={s.email} variant={'body2'}>
-          {email}
-        </Typography>
-        <Button variant={'secondary'}>
-          <Logout className={s.logout} onClick={handleLogout} />
-          Logout
-        </Button>
+        <div className={s.name}>{profileName}</div>
+        {saveChanges}
       </Card>
-    </>
+    </div>
   )
 }
-
-// let [editMode, setEditMode] = useState(false);
-// let [title, setTitle] = useState(value);
-//
-// const activateEditMode = () => {
-//     setEditMode(true);
-//     setTitle(value);
-// };
-// const activateViewMode = () => {
-//     setEditMode(false);
-//     onNameChange(title);
-// };
-// const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
-//     setTitle(e.currentTarget.value);
-// };
-//
-// let editableTypography = editMode ? <Typography variant={'h1'} className={s.name} onChange={changeTitle} autoFocus onBlur={activateViewMode}>
-//         {title}
-//     </Typography>
-//     :
-//     <span onDoubleClick={activateEditMode}>{value}</span>
-//
