5c8c3397b262d3552c877e4cd4615bb22579bc8f
Siarheispivak <siarheispivak2@gmail.com>
2024-03-29 11:20:13 +0300

    feat: useCreateDeckMutation was added. onSubmitModalHandler was added. fix:visualModal useState name was changed. fix:<AddDecksModal></AddDecksModal> component was wrapped with <Modal></Modal> component. fix:<AddDecksModal></AddDecksModal> component props were changed.

--- src/features/decks/ui/deck-add/deck-add.tsx
+++ src/features/decks/ui/deck-add/deck-add.tsx
@@ -1,35 +1,53 @@
 import { useState } from 'react'
 
+import { FormValuesCreateDeck } from '@/features/decks/lib/ui/create-deck-form/create-deck-form'
 import { AddDecksModal } from '@/pages/modals/add-decks-modal'
+import { useCreateDeckMutation } from '@/shared/services/decks-api'
 import { Button } from '@/shared/ui/button'
+import { Modal } from '@/shared/ui/modal/modal'
 import { Typography } from '@/shared/ui/typography'
 
 import s from './deck-add.module.scss'
 
 export const DeckAdd = () => {
-  const [visualModal, setVisualModal] = useState(false)
-  const [editPhoto, setEditPhoto] = useState(false)
+  const [isOpenModal, setIsOpenModal] = useState(false)
+  const [createDeck] = useCreateDeckMutation()
+  const onOpenChangeModal = (open: boolean) => {
+    setIsOpenModal(open)
+  }
+
+  // понять принцип работы
+  const onSubmitModalHandler = (data: FormValuesCreateDeck) => {
+    const newFormData = new FormData()
+
+    newFormData.append('name', data.name)
+    if (data?.cover) {
+      newFormData.append('cover', data.cover[0])
+    }
+    if (data?.isPrivate) {
+      newFormData.append('isPrivate', JSON.stringify(data.isPrivate))
+    }
+
+    createDeck(newFormData)
+      .unwrap()
+      .then(() => {
+        setIsOpenModal(false)
+      })
+  }
 
   return (
     <>
       <div className={s.container}>
         <Typography variant={'large'}>Decks list</Typography>
-        <Button
-          onClick={() => {
-            setVisualModal(true)
-            setEditPhoto(false)
-          }}
-          variant={'primary'}
-        >
-          Add New Deck
-        </Button>
-        <AddDecksModal
-          editPhoto={editPhoto}
-          setEditPhoto={setEditPhoto}
-          setVisitModal={setVisualModal}
-          visitModal={visualModal}
-        />
+        <Button onClick={() => setIsOpenModal(true)}>Add New Deck</Button>
       </div>
+      <Modal onOpenChange={onOpenChangeModal} open={isOpenModal}>
+        <AddDecksModal
+          onSubmit={onSubmitModalHandler}
+          setIsOpenModal={setIsOpenModal}
+          submitTextButton={'Create Deck'}
+        />
+      </Modal>
     </>
   )
 }
