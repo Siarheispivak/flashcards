8cee5570f18a4cfc8b292c1a1a042691c5a3170c
Siarheispivak <siarheispivak2@gmail.com>
2024-03-29 11:13:08 +0300

    feat:useController was injected to ControlledCheckbox component.

--- src/shared/ui/controlled/controlled-checkbox/controlled-checkbox.tsx
+++ src/shared/ui/controlled/controlled-checkbox/controlled-checkbox.tsx
@@ -1,17 +1,25 @@
-import {FieldValues, useController, UseControllerProps} from "react-hook-form";
-import {Checkbox, CheckboxProps} from "@/shared/ui/checkbox";
+import { FieldValues, UseControllerProps, useController } from 'react-hook-form'
 
+import { Checkbox, CheckboxProps } from '@/shared/ui/checkbox'
 
-type Props<T extends FieldValues> = Omit<UseControllerProps<T>,'rules' | 'defaultValue'> & Omit<CheckboxProps,'checked' | 'onValueChange'>
+type Props<T extends FieldValues> = Omit<UseControllerProps<T>, 'defaultValue' | 'rules'> &
+  Omit<CheckboxProps, 'checked' | 'onValueChange'>
 
-export const ControlledCheckbox = <T extends FieldValues>({control, name,shouldUnregister,disabled, ...rest}: Props<T>) => {
-    const {
-        field: {value, onChange},
-    } = useController({
-        name,
-        control,
-        disabled,
-        shouldUnregister
-    })
-    return <Checkbox  checked={value} disabled={disabled} onValueChange={onChange} {...rest}/>
+export const ControlledCheckbox = <T extends FieldValues>({
+  control,
+  disabled,
+  name,
+  shouldUnregister,
+  ...rest
+}: Props<T>) => {
+  const {
+    field: { onChange, value },
+  } = useController({
+    control,
+    disabled,
+    name,
+    shouldUnregister,
+  })
+
+  return <Checkbox {...rest} checked={value} disabled={disabled} onValueChange={onChange} />
 }
